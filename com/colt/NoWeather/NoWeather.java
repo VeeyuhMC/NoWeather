7e3b788a4f35cd37bb167692c4d7cf627ff96f42
Colt <Cewlt@users.noreply.github.com>
2015-01-18 14:32:03 -0500

    Update NoWeather.java

--- com/colt/NoWeather/NoWeather.java
+++ com/colt/NoWeather/NoWeather.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,19 +13,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class NoWeather extends JavaPlugin implements Listener{
 	
 	public void onEnable() {
-	saveDefaultConfig();
+		saveDefaultConfig();
         Bukkit.getPluginManager().registerEvents(this, this);
 	}
 	
-	@EventHandler
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void rain(WeatherChangeEvent e) {
 	  List<String> worlds = getConfig().getStringList("worlds");
 	  for(String w : worlds) {
-	    if(e.getWorld().equals(w)) {
+		World world = Bukkit.getWorld(w);
+		if(e.getWorld().equals(world)) {
 	      e.setCancelled(true);
 		  World ww = Bukkit.getServer().getWorld(w);
 		  ww.setStorm(false);
-	    }
+		}
 	  }
 	}
 }
